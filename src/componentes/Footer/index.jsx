ab54a779ccc86aff9e9529be416081f2730b49d2
Cpjm89 <moos892000@gmail.com>
2025-01-13 23:50:33 -0600

    linksRS

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -4,13 +4,13 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footerColor.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.facebook.com/josemiguel.cruzpalomares/'>
                 <img src='/img/facebook.png' alt="Facebook" />
             </a>
-            <a href='https://www.aluracursos.com/'>
-                <img src="/img/twitter.png" alt='twitter' />
+            <a href='https://github.com/Cpjm89/'>
+                <img src="/img/twitter.png" alt='GitHub' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/josemiguelcruzpalomares?igsh=eGZlb25tdzM5NjVt/'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
