b2692c99a8ed2435aeca31cc9645086b3e7c95b9
Cpjm89 <moos892000@gmail.com>
2025-01-14 00:01:14 -0600

    enlaceFB

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -4,8 +4,8 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footerColor.png)" }}>
         <div className='redes'>
-            <a href='https://www.facebook.com/josemiguel.cruzpalomares/'>
-                <img src='/img/facebook.png' alt="Facebook" />
+            <a href="https://www.facebook.com/josemiguel.cruzpalomares/">
+                <img src="/img/facebook.png" alt="Facebook" />
             </a>
             <a href='https://github.com/Cpjm89/'>
                 <img src="/img/twitter.png" alt='GitHub' />
